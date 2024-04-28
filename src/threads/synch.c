cbbad6f654a485227eaac62a2f7911a75454cd09
RafikSameh <rafiksameh123@gmail.com>
2024-04-29 02:44:09 +0300

    rafik/fixing_bugs_synch.c

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -353,6 +353,7 @@ cond_signal (struct condition *cond, struct lock *lock UNUSED)
     list_sort(&cond->waiters,cond_sema_cmp_priority,NULL);
     sema_up (&list_entry (list_pop_front (&cond->waiters),
                           struct semaphore_elem, elem)->semaphore);
+  }
 }
 
 /* Wakes up all threads, if any, waiting on COND (protected by
